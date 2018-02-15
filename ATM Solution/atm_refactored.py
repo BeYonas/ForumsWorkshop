a7da1902036da4910d86337a4d63c4884d54859a
Unknown <badachyounes@gmail.com>
2018-02-15 16:26:38 +0100

    Updating ATM solution code implementing a function to withdraw

---
+++ ATM Solution/atm_refactored.py
@@ -0,0 +1,39 @@
+# allowed papers: 100, 50, 10, 5, and cents
+
+balance = 500
+
+request = 275
+
+
+def withdraw(balance, request):
+    print "your balance is :", balance
+    if request > balance:
+        print("Can't give you all this money !!")
+    elif request < 0:
+        print("More than zero plz!")
+
+    else:
+        balance -= request
+        while request > 0:
+
+            if request >= 100:
+                request -= 100
+                print("give 100")
+
+            elif request >= 50:
+                request -= 50
+                print("give 50")
+
+            elif request >= 10:
+                request -= 10
+                print("give 10")
+
+            elif request >= 5:
+                request -= 5
+                print("give 5")
+
+            elif request < 5:
+                print("give " + str(request))
+                request = 0
+    return balance
+withdraw(balance, request)
