22933f2449a33c9c9c4f0074e5617b0b9fef47dc
Unknown <badachyounes@gmail.com>
2018-02-23 22:49:29 +0100

    Python workshop Lesson 13
    Python workshop Lesson 13 : Creating "models" module for initializing entities classes Post and Member and importing them into "main" module

---
+++ Forums/main.py
@@ -0,0 +1,8 @@
+import models
+
+member1 = models.Member("member1", 1)
+member2 = models.Member("member2", 2)
+
+post1 = models.Post("title1", "content1")
+post2 = models.Post("title2", "content2")
+post3 = models.Post("title3", "content3")
