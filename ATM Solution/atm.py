f8199946ef06ed59de087b1b12852d740254bfa2
Unknown <badachyounes@gmail.com>
2018-02-23 17:18:45 +0100

    Python workshop Lesson 8 : Updating the ATM solution by implementing classe ATM and upgrading the withdraw function by implementing it into the ATM classe as a classe method and to print bank_name

--- ATM Solution/atm.py
+++ ATM Solution/atm.py
@@ -1,24 +1,54 @@
-# allowed papers: 100, 50, 10, 5, and rest of request
 
-money = 500
 
-request = 277
+class ATM:
+    def __init__(self, balance, bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
 
-if money >= request > 0:
-    while request >= 100:
-        print "give 100"
-        request -= 100
-    while request >= 50:
-        print "give 50"
-        request -= 50
-    while request >= 10:
-        print "give 10"
-        request -= 10
-    if request >= 5:
-        print "give 5"
-        request -= 5
-    if request > 0:
-        print "give " + str(request)
-        request = 0
-else:
-    print "Invalid request !"
+    def withdraw(self, request):
+        print "Welcome to", self.bank_name
+        print "Current balance =", self.balance
+        print "=================================="
+        if request > self.balance:
+            print("Can't give you all this money !!")
+        elif request < 0:
+            print("More than zero plz!")
+
+        else:
+            self.balance -= request
+            while request > 0:
+
+                if request >= 100:
+                    request -= 100
+                    print("give 100")
+
+                elif request >= 50:
+                    request -= 50
+                    print("give 50")
+
+                elif request >= 10:
+                    request -= 10
+                    print("give 10")
+
+                elif request >= 5:
+                    request -= 5
+                    print("give 5")
+
+                elif request < 5:
+                    print("give " + str(request))
+                    request = 0
+        print "=================================="
+        return self.balance
+
+
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm2.withdraw(2000)
