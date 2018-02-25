5b6cf3b6f71dee50b347a59e1d626af72bdd5cb0
Unknown <badachyounes@gmail.com>
2018-02-25 19:41:04 +0100

    Python workshop Lesson 17
    Implementing the get_by_id and delete and entity_exists into the stores classes

--- Forums/main.py
+++ Forums/main.py
@@ -1,10 +1,10 @@
 import models
-import store
+import stores
 
 member1 = models.Member("member1", 1)
 member2 = models.Member("member2", 2)
 
-members_store = store.MemberStore()
+members_store = stores.MemberStore()
 members_store.add(member1)
 members_store.add(member2)
 
@@ -12,7 +12,7 @@ post1 = models.Post("title1", "content1")
 post2 = models.Post("title2", "content2")
 post3 = models.Post("title3", "content3")
 
-posts_store=store.MemberStore()
+posts_store = stores.MemberStore()
 posts_store.add(post1)
 posts_store.add(post2)
 posts_store.add(post3)
--- Forums/stores.py
+++ Forums/stores.py
@@ -1,6 +1,5 @@
-
 class MemberStore:
-
+    Last_id = 1
     members = []
 
     def get_all(self):
@@ -9,22 +8,35 @@ class MemberStore:
 
     def add(self, member):
         # append member
+        member.id = MemberStore.Last_id
+
         MemberStore.members.append(member)
+
+        MemberStore.Last_id += 1
 
     def get_by_id(self, id):
         # search for member by id
+        for member in MemberStore.members:
+            if member.id == id:
+                return member
+        return None
 
     def update(self, member):
         # update member data
+        pass
 
     def delete(self, id):
         # delete member by id
+        member_to_delete = MemberStore.get_by_id(id)
+        MemberStore.members.remove(member_to_delete)
 
     def entity_exists(self, member):
         # checks if an entity exists in a store
+        return member in MemberStore.members
+
 
 class PostStore:
-
+    Last_id = 1
     posts = []
 
     def get_all(self):
@@ -33,16 +45,28 @@ class PostStore:
 
     def add(self, post):
         # append post
+        post.id = PostStore.Last_id
+
         PostStore.posts.append(post)
+
+        PostStore.Last_id += 1
 
     def get_by_id(self, id):
         # search for post by id
+        for post in PostStore.posts:
+            if post.id == id:
+                return post
+        return None
 
     def update(self, post):
         # update post data
+        pass
 
     def delete(self, id):
         # delete post by id
+        post_to_delete = PostStore.get_by_id(id)
+        PostStore.posts.remove(post_to_delete)
 
     def entity_exists(self, post):
         # checks if an entity exists in a store
+        return post in PostStore.posts
