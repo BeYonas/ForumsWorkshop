6a253568b33e9889705deb60b6d92fc99447f044
Yonas-Be <36481484+BeYonas@users.noreply.github.com>
2018-03-08 16:38:57 +0100

    Python workshop Lesson 25: Simple flask demo

---
+++ flash-demo/flask-demo.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def home():
+     return "Hi there"
+
+@app.route("/SayHello/<name>")
+def say_hellow(name):
+    return "Hello {name}".format(name=name)
+
+
+app.run()
